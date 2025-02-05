aaf0d353b64b1b5c3be6fda6738cf0bde969f1b5
Vatsal Dave <vatsaldave2002@gmail.com>
2025-02-05 15:10:10 +0530

    test: remove App.test.tsx

--- src/tests/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-test("render App", () => {
-  render(<App />);
-  const header = screen.getByRole("heading", { name: "Vite + React" });
-  expect(header).toBeInTheDocument();
-});
